69353fdffc32baab69a94a3e9a6dc3c93b6ee0dd
BC <boc5244@psu.edu>
2019-01-10 12:30:12 -0500

    Update WingTracker.m

--- Wing Tracker/WingTracker.m
+++ Wing Tracker/WingTracker.m
@@ -6,61 +6,62 @@ function [] = WingTracker(vidData,debug)
     % Outputs:
         % WingAngles: structure containing L & R wing angles and mask points
         % debug:
-    %%
-    [lCenterPos, rCenterPos, lMaskPts, rMaskPts,rtop,rbot,ltop,lbot] = MakeWingMask(vidData);
-    threshold = 20;
-    rPoly = impoly(gca, rMaskPts);
-    lPoly = impoly(gca, lMaskPts);
-    rMask = createMask(rPoly);                      
-    lMask = createMask(lPoly);                      % making the mask
+%% Area Tracking %%
+%---------------------------------------------------------------------------------------------------------------------------------
+[lCenterPos, rCenterPos, lMaskPts, rMaskPts,rtop,rbot,ltop,lbot] = MakeWingMask(vidData);
+rPoly = impoly(gca, rMaskPts);
+lPoly = impoly(gca, lMaskPts);
+rMask = createMask(rPoly);
+lMask = createMask(lPoly);
 
-    rlow = atan2d(rbot(2)-rCenterPos(2),rbot(1)-rCenterPos(1));
-    llow = atan2d_adv(lbot(2)-lCenterPos(2),lbot(1)-lCenterPos(1));
-    rhigh = atan2d(rtop(2)-rCenterPos(2),rtop(1)-rCenterPos(1));
-    lhigh = atan2d_adv(ltop(2)-lCenterPos(2),ltop(1)-lCenterPos(1)); 
-    rrange = rhigh-rlow;
-    lrange = lhigh-llow;                % find the angle ranges
+rlow = atan2d(rbot(2)-rCenterPos(2),rbot(1)-rCenterPos(1));
+llow = atan2d_adv(lbot(2)-lCenterPos(2),lbot(1)-lCenterPos(1));
+rhigh = atan2d(rtop(2)-rCenterPos(2),rtop(1)-rCenterPos(1));
+lhigh = atan2d_adv(ltop(2)-lCenterPos(2),ltop(1)-lCenterPos(1)); 
+rrange = rhigh-rlow;
+lrange = lhigh-llow;
 
-    Vid = squeeze(vidData);
-    thresh.low  = 15;
-    thresh.high = 100;
-    SE.erode =  strel('disk',3,8); % shape to use for erode and dilate functions
-    SE.dilate = strel('disk',3,8); % shape to use for erode and dilate functions
-    tic
-    for kk = 1:2000
-        clear Frame
-        DISP = Vid(:,:,kk); % get frame
-        IMG{1} = DISP;
-        IMG{2} = medfilt2(IMG{1}); % median filter
-        IMG{3} = IMG{2};
-        thresh.Idx = (IMG{3} >= thresh.low) & (IMG{3} <= thresh.high); % find pixels in range
-        IMG{3}(thresh.Idx) = 1; % convert to bianary above threshold
-        IMG{3}(~thresh.Idx) = 0;  % convert to bianary below threshold
-        IMG{3} = logical(IMG{3});
-        IMG{4} = medfilt2(IMG{3}); % median filter
-        IMG{5} = imfill(IMG{4},'holes'); % fill in small holes
-        IMG{6} = imerode(IMG{5},SE.erode); % dilate
-        IMG{7} = imdilate(IMG{6},SE.dilate); % erode
-        IMG{8} = bwareaopen(IMG{7}, 5000); % get rid of disconnected objects
-        Frame = IMG{8};
+Vid = squeeze(vidData);
+thresh.low  = 15;
+thresh.high = 100;
+SE.erode =  strel('disk',3,8); % shape to use for erode function
+SE.dilate = strel('disk',3,8); % shape to use for dilate function
 
-        f = Frame;
-        rAngles(kk) = calculateAngle(f, rMask, rrange, rlow);     
-        lAngles(kk)= calculateAngle(f, lMask, lrange, llow);   % Calculate the actual angle for the left and right wings
+tic
+for kk = 1:2000
+    clear Frame
+    DISP = Vid(:,:,kk); % get frame
+    IMG{1} = DISP;
+    IMG{2} = medfilt2(IMG{1}); % median filter
+    IMG{3} = IMG{2};
+    thresh.Idx = (IMG{3} >= thresh.low) & (IMG{3} <= thresh.high); % find pixels in range
+    IMG{3}(thresh.Idx) = 1; % convert to bianary above threshold
+    IMG{3}(~thresh.Idx) = 0;  % convert to bianary below threshold
+    IMG{3} = logical(IMG{3});
+    IMG{4} = medfilt2(IMG{3}); % median filter
+    IMG{5} = imfill(IMG{4},'holes'); % fill in small holes
+    IMG{6} = imerode(IMG{5},SE.erode); % dilate
+    IMG{7} = imdilate(IMG{6},SE.dilate); % erode
+    IMG{8} = bwareaopen(IMG{7}, 5000); % get rid of disconnected objects
+    Frame = IMG{8};
 
-        if debug
-         figure (2)
-         imshow(IMG{1})
-         hold on
-         hLeft  = animatedline('Color','g','LineWidth',2);
-         hRight = animatedline('Color','r','LineWidth',2);
-         addpoints(hLeft, lCenterPos(1),lCenterPos(2));
-         addpoints(hRight, rCenterPos(1),rCenterPos(2));
-         addpoints(hLeft, lCenterPos(1)-100,lCenterPos(2)-tand(lAngles(kk))*100);
-         addpoints(hRight,rCenterPos(1)+100,rCenterPos(2)+tand(rAngles(kk))*100); % plot the angles and see if that is correct
-        end
+    f = Frame;
+    rAngles(kk) = calculateAngle(f, rMask, rrange, rlow);     
+    lAngles(kk)= calculateAngle(f, lMask, lrange, llow);   % Calculate the actual angle for the left and right wings
+
+    if debug
+     figure (2)
+     imshow(IMG{1})
+     hold on
+     hLeft  = animatedline('Color','g','LineWidth',2);
+     hRight = animatedline('Color','r','LineWidth',2);
+     addpoints(hLeft, lCenterPos(1),lCenterPos(2));
+     addpoints(hRight, rCenterPos(1),rCenterPos(2));
+     addpoints(hLeft, lCenterPos(1)-100,lCenterPos(2)-tand(lAngles(kk))*100);
+     addpoints(hRight,rCenterPos(1)+100,rCenterPos(2)+tand(rAngles(kk))*100); % plot the angles and see if that is correct
     end
-    toc
+end
+toc
 
 end
 %% Internal Function Definitions %%
