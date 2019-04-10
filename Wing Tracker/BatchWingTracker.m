9084b5a14be4ffd43fb7e2fb296e66af9fd0abf0
boc5244 <31375763+boc5244@users.noreply.github.com>
2019-04-10 00:39:50 -0400

    Update BatchWingTracker.m

--- Wing Tracker/BatchWingTracker.m
+++ Wing Tracker/BatchWingTracker.m
@@ -3,41 +3,56 @@
 %---------------------------------------------------------------------------------------------------------------------------------
 clear;close all;clc
 
-root = 'C:\Users\boc5244\Documents\Vid\';
+root = 'H:\EXPERIMENTS\Experiment_Asymmetry_Control_Verification\HighContrast\0\Vid\';
 
 [files, dirpath] = uigetfile({'*.mat', 'DAQ-files'}, ... % select video files
     'Select fly trials', root, 'MultiSelect','on');
-
-if ischar(files)
-    FILES{1} = files;
-else
-    FILES = files;
-end
+FILES = cellstr(files)';
 clear files
 
 nTrial = length(FILES); % total # of trials
 
 %% Load video data & run tracking software %%
 %---------------------------------------------------------------------------------------------------------------------------------
+close all
 for jj = 1:nTrial
     % Load video data
     load([dirpath FILES{jj}]); % load video data
     disp('Load File: Done')
     
     % Set tracking parametrs
-    debug = 0;
+    debug = true;
+    
+    % Make Mask
+%     [Wing.Mask] = MakeWingMask(vidData);
     
     % Run tracking
-    Wing = WingTracker(vidData,debug);
+    tic
+%     [lAngles, rAngles] = curvedWingEdgeAnalyzer_V3(vidData, Wing.Mask.R.points, Wing.Mask.L.points, ...
+%         Wing.Mask.R.center, Wing.Mask.L.center, 0.25, debug);
+    [lAngles, rAngles, lCenterPos, rCenterPos, lMask, rMask] = curvedWingEdge(vidData, 0.3, debug, '');
+    toc
+    
     Wing.Time = t_v;
+    Wing.Ang.L = lAngles;
+    Wing.Ang.R = rAngles;
+    
+    % Hampel filter
+	Wing.Ang.hL = hampel(Wing.Time, Wing.Ang.L, 50, 4);
+    Wing.Ang.hR = hampel(Wing.Time, Wing.Ang.R, 50, 4);
+    
+    % Plot angles
+    figure (1) ; clf ; hold on
+    plot(Wing.Time,Wing.Ang.L,'r')
+    plot(Wing.Time,Wing.Ang.R,'g')
+	plot(Wing.Time,Wing.Ang.hL,'m')
+    plot(Wing.Time,Wing.Ang.hR,'c')
+
+    pause
     
     % Save data
     disp('Save Data...')
-    save([dirpath 'WingAngles\' FILES{jj} ],'-v7.3','Wing');
-    
-%     close all
-%     clc
-
-    pause
-
+    save([dirpath 'WingAngles\' FILES{jj} ],'-v7.3','Wing');  
 end
+
+
